f3602385227d6d2c09cf6997da5f0894cf5d65f7
Luke <luca.camilotti@gmail.com>
2021-01-08 08:58:57 +0100

    Second commit: changes to MainActivity

--- app/src/main/java/com/camilotti/mygithubapplication/MainActivity.java
+++ app/src/main/java/com/camilotti/mygithubapplication/MainActivity.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean pippo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pippo = false;
     }
 }
